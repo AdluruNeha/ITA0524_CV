cf5f841b100c8bbd57d7dc8354e86df5de9053c1
AdluruNeha <122555691+AdluruNeha@users.noreply.github.com>
2024-09-17 13:20:03 +0530

    Delete ex2-gaussian_blur.py

--- ex2-gaussian_blur.py
+++
@@ -1,9 +0,0 @@
-import cv2
-path = r"D:\shifted_C\New folder\nature1.jpeg"
-img = cv2.imread(path)
-bluredimg = cv2.GaussianBlur(img,(3,3),0)
-cv2.imshow("Original image: ",img)
-cv2.imshow("Blured image: ",bluredimg)
-cv2.waitKey(0)
-cv2.destrayAllWindows()
-                 
